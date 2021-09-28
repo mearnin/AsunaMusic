e37f044750ae8357ad4b9ca57db8b9bf3d600f86
sathaaaaaa <85862925+sathaaaaaa@users.noreply.github.com>
2021-09-28 21:51:12 +0530

    Create __main__.py

---
+++ AsunaMusic/__main__.py
@@ -0,0 +1,28 @@
+import os
+from pyrogram import Client, idle
+from config import API_ID, API_HASH, BOT_TOKEN, SESSION_STRING
+
+USER = Client(
+    SESSION_STRING,
+    API_ID,
+    API_HASH
+)
+
+Bot = Client(
+    ":memory:",
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="plugins"),
+)
+if not os.path.isdir("./downloads"):
+    os.makedirs("./downloads")
+
+Bot.start()
+User.start()
+print("\n[INFO] - STARTED VIDEO PLAYER BOT, JOIN @ASMSAFONE !")
+
+idle()
+Bot.stop()
+User.stop()
+print("\n[INFO] - STOPPED VIDEO PLAYER BOT, JOIN @ASMSAFONE !")
