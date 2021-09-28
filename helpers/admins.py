e99fca50e10a2040aebc60c164935478cd71d699
mearnin <87330597+mearnin@users.noreply.github.com>
2021-09-28 21:01:59 +0530

    Add files via upload

---
+++ helpers/admins.py
@@ -0,0 +1,34 @@
+from typing import List
+
+from pyrogram.types import Chat
+
+from typing import Dict, List
+
+admins: Dict[int, List[int]] = {}
+
+
+def set(chat_id: int, admins_: List[int]):
+    admins[chat_id] = admins_
+
+
+def gett(chat_id: int) -> List[int]:
+    if chat_id in admins:
+        return admins[chat_id]
+    return []
+
+
+async def get_admins(chat: Chat) -> List[int]:
+    get = gett(chat.id)
+
+    if get:
+        return get
+    else:
+        administrators = await chat.get_members(filter="administrators")
+        to_set = []
+
+        for administrator in administrators:
+            if administrator.can_manage_voice_chats:
+                to_set.append(administrator.user.id)
+
+        set(chat.id, to_set)
+        return await get_admins(chat)
